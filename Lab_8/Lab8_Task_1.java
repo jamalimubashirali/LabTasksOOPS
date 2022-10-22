7a59170fb23cfd1900652123e7c2c9dbcdd55086
Mubashir <21sw071@students.muet.edu.pk>
2022-10-22 16:19:40 +0500

    Lab 8 Tasks Added

--- Lab_8/Lab8_Task_1.java
+++ Lab_8/Lab8_Task_1.java
@@ -1,5 +1,30 @@
 package Lab_8;
 
-public class Lab8_Task_1 {
+class MyThread extends Thread{
+    private int a;
 
+    @Override
+    public void run() {
+        super.run();
+        int i=1;
+        while(i>0){
+            a+=2;
+            System.out.print(a+" ");
+            i++;
+            if(i%30==0){
+                System.out.println();
+                try{
+                    Thread.sleep(5000);
+                }catch (InterruptedException e){
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
+public class Lab8_Task_1 {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
 }
--- Lab_8/Lab8_Task_2.java
+++ Lab_8/Lab8_Task_2.java
@@ -1,4 +1,44 @@
 package Lab_8;
+class Storage{
+    public static int counter;
+}
+class Counter extends Thread {
+    @Override
+    public void run() {
+        super.run();
+        for(int i=0;i>=0;i++){
+            Storage.counter=i;
+            try{
+                Thread.sleep(300);
+            }catch (InterruptedException e){
+                System.out.println(e);
+            }
+        }
 
+    }
+}
+
+class Printer extends Thread{
+    Storage store = new Storage();
+    @Override
+    public void run() {
+
+        super.run();
+        for(int i=0;i>=0;i++){
+            System.out.print(Storage.counter+" ");
+            try{
+                Thread.sleep(300);
+            }catch (InterruptedException e){
+                System.out.println(e);
+            }
+        }
+    }
+}
 public class Lab8_Task_2 {
+    public static void main(String[] args) {
+        Counter count = new Counter();
+        Printer print = new Printer();
+        print.start();
+        count.start();
+    }
 }
